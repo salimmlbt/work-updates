054aec3b379a08d7bbe986be61e7ab1e7885439d
Muhammad Salim <salimmullambath@gmail.com>
2025-10-14 15:12:02 +0000

    ## Error Type Runtime Error
    ## Error Message
    Body exceeded 1 MB limit.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  serverActions: {
+    bodySizeLimit: '10mb',
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
