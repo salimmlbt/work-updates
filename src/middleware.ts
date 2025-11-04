6af457d5e6b604e540bf7c1b38e9024b2d357440
Muhammad Salim <salimmullambath@gmail.com>
2025-11-04 09:38:39 +0000

    ## Error Type Console SecurityError
    ## Error Message
    Failed to register

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - sw.js (service worker)
+     * - manifest.json (web app manifest)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|sw.js|manifest.json|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
